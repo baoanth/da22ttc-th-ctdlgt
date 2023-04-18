e6d04e9fdd863c0ee53edeeab429316ddb696c9c
kieugiathinh <kieugiathinh1301@gmail.com>
2023-04-18 10:04:12 +0700

    Update writing structs to file

--- kieu_gia_thinh/write_struct.c
+++ kieu_gia_thinh/write_struct.c
@@ -10,3 +10,20 @@ struct person
     char fname[20];
     char lname[20];
 };
+
+//Ham  main
+int main()
+{
+	struct person per1 = {1, "Thinh", "Kieu"};
+	struct person per2 = {1, "My", "Nguyen"};
+	
+	FILE *outfile = fopen("person.dat", "w");
+	
+	fwrite(&per1, sizeof(struct person), 1, outfile);
+	fwrite(&per2, sizeof(struct person), 1, outfile);
+	
+	if(fwrite!=0) printf("Ghi file thanh cong roi!");
+	else printf("LOI! Gi file KHONG thanh cong!");
+	
+	fclose(outfile);
+}
