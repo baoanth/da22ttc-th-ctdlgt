c65afad98d9636679766d12da095e11edc8d21c6
nhan <nhanhuynhtv345@gmail.com>
2023-04-18 22:30:35 +0700

    Update writing structs to file

--- huynh_quoc_nhan/write_struct.c
+++ huynh_quoc_nhan/write_struct.c
@@ -10,3 +10,19 @@ struct person
     char fname[20];
     char lname[20];
 };
+int main()
+{
+	struct person per1 = {1,"Chau","Qui"};
+	struct person per2 = {2,"Tran","Tho"};	
+	
+	FILE* outfile = fopen("person.dat","w");
+	
+	fwrite(&per1, sizeof(struct person), 1, outfile);
+	fwrite(&per2, sizeof(struct person), 2, outfile);	
+	
+	if (fwrite!=0)
+	  printf("Write file successfully");
+	else
+	  printf("ERROR! Write file unsuccessfully");
+	fclose(outfile);    
+}
