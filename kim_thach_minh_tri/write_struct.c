9adac70fc513d2959e41a5edf21e929484244a1c
kim_thach_minh_tri <trikimfa1904@gmail.com>
2023-04-18 09:56:20 +0700

    Finish Writing Struct

--- kim_thach_minh_tri/write_struct.c
+++ kim_thach_minh_tri/write_struct.c
@@ -4,17 +4,17 @@
 #include <stdlib.h>
 
 // struct person with 3 fields
-typedef struct 
+ struct person
 {
     int id;
     char fname[20];
     char lname[20];
-} person;
+} ;
 
 
 int main()
 {
-    FILE *outline;
+   /* FILE *outline;
     person per1, per2;
     per1.id=1;
     per2.id=2;
@@ -25,6 +25,21 @@ int main()
     outline = fopen ("person.dat","w");
     fprintf(outline, "%d %s %s\n", per1.id, per1.fname, per1.lname);
     fprintf(outline, "%d %s %s\n", per2.id, per2.fname, per2.lname);
+   */
+    struct person per1 = {1, "Kim" , "Tri" };
+	struct person per2 = {2, "Thach" , "Minh" };	
+	
+	FILE *outfile = fopen("person.dat", "w");	
+	
+	fwrite(&per1, sizeof(struct person), 1, outfile);
+	fwrite(&per2, sizeof(struct person), 1, outfile);	
+	
+	if (fwrite!=0)
+		printf("Ghi file thanh cong roi!");
+	else
+		printf("LOI!! Ghi file KHONG thanh cong!");	
+	
+	fclose(outfile); 
    return 0; 
 }
  
