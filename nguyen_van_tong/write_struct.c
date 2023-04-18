2e31a9372a9ff91dc6de28d7d860c5c1288a9857
TongNguyenvk <tongct08@gmail.com>
2023-04-18 09:02:03 +0700

    Update writing structs to file

--- nguyen_van_tong/write_struct.c
+++ nguyen_van_tong/write_struct.c
@@ -13,17 +13,20 @@ struct person
 };
 int main(){
 person per1 , per2;
-
+per1.id =1;
 strcpy("Nguyen", per1.fname);
 strcpy("A",per1.lname);
+per2.id = 2;
 strcpy("LE",per2.fname );
 strcpy("B",per2.lname);
 FILE *f;
 f= fopen("person.dat","w");
 if(f!=NULL){
 printf("da mo duoc file");
+fprintf(f,"ma so per1 %d\n", per1.id);
 fprintf(f," ho per1 %s\n", per1.fname);
 fprintf(f," ten per1 %s\n", per1.lname);
+fprintf(f,"ma so per2 %d\n",per2.id);
 fprintf(f," ho per2 %s\n", per2.fname);
 fprintf(f," ten per2 %s\n", per2.lname);
 }
