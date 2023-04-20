3b7f54bf14c831107fcfe31ecf61833f9a232089
thoaithuong <thuongthach.291204@gmail.com>
2023-04-20 14:51:07 +0700

    Update writing structs to file

--- thach_ngoc_hoai_thuong/write_struct.c
+++ thach_ngoc_hoai_thuong/write_struct.c
@@ -1,8 +1,6 @@
-// C program for reading
-// struct from a file
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 // struct person with 3 fields
 struct person
 {
@@ -10,3 +8,21 @@ struct person
     char fname[20];
     char lname[20];
 };
+//Ham main
+int main()
+{
+    struct person per1 ={1,"Huynh", "Lac"};
+    struct person per2 ={2,"Hoai", "Thuong"};
+     struct person per3 ={3,"Cam", "Lan"};
+    FILE *outfile = fopen("person.dat","w");
+    fwrite(&per1, sizeof(struct person),1,outfile);
+    fwrite(&per2, sizeof(struct person),1,outfile);
+    fwrite(&per3, sizeof(struct person),1,outfile);
+    if (fwrite!=0)
+    printf("Ghi file thanh cong roi do nha!");
+    else printf("LOI!! Ghi file khong thanh cong");
+    fclose(outfile);
+     return 0;
+    
+    
+}
