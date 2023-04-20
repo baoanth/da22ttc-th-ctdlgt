78dba87e4bfe9d1db7244cf9d89e7379d6ef916f
nganphamGD <nganpham4625@anpham4625gmail.com>
2023-04-20 11:28:27 +0700

    Update writing structs to file

--- Phạm_Thị_Kim_Ngân/wirte_struct.c
+++ Phạm_Thị_Kim_Ngân/wirte_struct.c
@@ -14,15 +14,16 @@ int main()
 {
     printf"hewllo";
     printf"xin chao";
-}int main()
- {struct person per1 = {1,"pham","thi"};
- struct person per2 = {2,"kim","nan"};
- FILE* outfile = fopen("preson.dat","w");
- fwrite(&per1, sizeof(struct person),1,outfile);
- fwrite(&per2, sizeof(struct person),1,outfile);
- if (fwrite!=0) 
- printf(" ghi thanh cong");
- else
- printf("loi!");
- fclose(outfile);
- }
+
+}int  main ()
+{ struct nguoi per1 = { 1 , " pham " , " thi " };
+ struct nguoi per2 = { 2 , " kim " , " nan " };
+FILE* outfile = fopen ( " preson.dat " , " w " );
+ fwrite (&per1, sizeof ( struct person), 1 ,outfile);
+ fwrite (&per2, sizeof ( struct person), 1 ,outfile);
+ nếu ( fwrite != 0 )
+ printf ( " ghi thanh cong " );
+ khác
+ printf ( " loi! " );
+ fclose (tệp ngoài);
+}
