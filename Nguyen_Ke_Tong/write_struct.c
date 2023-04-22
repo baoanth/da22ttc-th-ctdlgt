2aa7e24ba33be34bd2226457aa8cd8b542807787
nguyenketong <nguyenketong1603@gmail.com>
2023-04-22 21:49:52 +0700

    Update

--- Nguyen_Ke_Tong/write_struct.c
+++ Nguyen_Ke_Tong/write_struct.c
@@ -18,7 +18,7 @@ int main()
 	FILE* outfile = fopen("person.dat","w");
 	
 	fwrite(&per1, sizeof(struct person), 1, outfile);
-	fwrite(&per2, sizeof(struct person), 2, outfile);	
+	fwrite(&per2, sizeof(struct person), 1, outfile);	
 	
 	if (fwrite!=0)
 	  printf("Write file successfully");
