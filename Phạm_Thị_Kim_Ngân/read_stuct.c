8109994bfb14708aaf34887f4af3e324b7a80adb
nganphamGD <nganpham4625@gmail.com>
2023-04-23 11:47:03 +0700

    My first commit on read_struct.c

---
+++ Phạm_Thị_Kim_Ngân/read_stuct.c
@@ -0,0 +1,12 @@
+// C program for reading
+// struct from a file
+#include &lt;stdio.h&gt;
+#include &lt;stdlib.h&gt;
+
+// struct person with 3 fields
+struct person
+{
+    int id;
+    char fname[20];
+    char lname[20];
+};
