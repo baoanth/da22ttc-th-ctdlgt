e91df7f5f322e3762d1685222d40f7b03083609c
nguyenketong <nguyenketong1603@gmail.com>
2023-04-26 08:12:48 +0700

    update tim hang hoa

---
+++ to_doan_nguyet_thuy/read_write_text_file.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void input_text_file(char*filename)
+{
+	FILE *outfile
+	char input_tr[MAX_LINE_LENGTH];
+	
+	
+	otfile =  fopen(filename, "w");
+	printf("Nhap van ban, an EXIT ngung nhap:\n ");
+	
+	
+	while(1)
+	{
+		fgets(input_tr, _MAX_LINE_LENGTH, stdin);
+		
+		
+		
+	
+	}
+
+    fclose(outfile);
+}
+
+
+
+	
+
+
+
+
+}
