29d43c4fcf6fb17714baf6874c8f3da9afe01044
huetrinh204 <trinhfokko@gmail.com>
2023-04-27 13:27:08 +0700

    cap nhat

--- annb/TH2_doc_ghi_file_nhi_phan/danh_sach_hang_hoa.c
+++ annb/TH2_doc_ghi_file_nhi_phan/danh_sach_hang_hoa.c
@@ -59,6 +59,12 @@ void print_HH(HangHoa h)
     printf("%10s%25s%10d%12f%12f\n", h.mahang, h.tenhang, h.soluong, h.gia, h.thanhtien);	
 }
 
+/*
+Ham tim kiem mot mahang trong danh sach hh_array
+Tim thay thi tra chi so cua mon hang trong hh_array
+Khong tim thay tra ve -1
+Mon hang duoc tim thay luu trong *kq
+*/
 int find_HH_by_ma(char* mahang, HangHoa* hh_array, int count, HangHoa* kq)
 {
     
@@ -71,13 +77,27 @@ int find_HH_by_ma(char* mahang, HangHoa* hh_array, int count, HangHoa* kq)
         if( strcmp(hh_array[i].mahang, mahang )==0)        
         {
         	*kq= hh_array[i];
-        	return 1;
+        	return i;
         }             
         i++;
 	}
-	return  0;
+	return  -1;
 }
 
+void input_DMHH(char* filename) 
+{
+   //Mo file doc noi dung dua vao mot mang my_dmhh
+
+   //Lap cho den khi nhap ma = EXIT
+   // Nhap mahang_tam 
+   // Neu mahang_tam == EXIT thi break
+   // Tim trong my_dmhh xem mahang_tam co chua, neu chua thif nhap binh thuong, neu da co thi in ra ten hang.
+   // Nhap gia, soluong, tinh thanhtien = soluong*dongia
+   // Dua mon hang vua nhap vao my_dmhh
+
+   // Luu toan bo my_dmhh vao outfile, dong outfile
+
+}
 
 
 int main()
