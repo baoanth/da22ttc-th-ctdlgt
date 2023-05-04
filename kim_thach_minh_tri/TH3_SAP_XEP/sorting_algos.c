de2ad0c84439317fcdf497bfdd1ca0fb3f1a6d60
kimthachminhtri <trikikfa1904@gmail.com>
2023-05-04 16:28:51 +0700

    First 4 sortng algos

--- kim_thach_minh_tri/TH3_SAP_XEP/sorting_algos.c
+++ kim_thach_minh_tri/TH3_SAP_XEP/sorting_algos.c
@@ -107,6 +107,27 @@ int i, j;
 		
 }
 
+void QuickSort(int a[], int l, int r)
+{
+	int i, j;
+	int x;
+	x = a[(l+r)/2]; 
+	i =l; j = r;
+	do {
+		while(a[i] < x) i++;
+		while(a[j] > x) j--;
+			if(i <= j)
+			{
+				Hoanvi(a[i],a[j]);
+				i++ ; j--;
+			}
+	}while(i < j);
+	if(l < j)
+	QuickSort(a,l,j);
+	if(i < r)
+	QuickSort(a,i,r);
+}
+
 
 int main()
 {
