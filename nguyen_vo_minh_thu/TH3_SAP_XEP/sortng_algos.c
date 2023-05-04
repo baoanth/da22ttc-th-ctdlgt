f051853339a6d6d4a72c9603844c5cf0fda2bd30
minhthu2204 <minhminhtv220403@gmail.com>
2023-05-04 16:27:01 +0700

    My first commit on read_struct.c

--- nguyen_vo_minh_thu/TH3_SAP_XEP/sortng_algos.c
+++ nguyen_vo_minh_thu/TH3_SAP_XEP/sortng_algos.c
@@ -31,7 +31,7 @@ void SelectionSort(int a[],int n)
 	{
 		min=i;
 		
-		for(int j=i+1; j<n; j++)
+		for( j=i+1; j<n; j++)
 			if(a[j] < a[min])
 				min = j;
 		hoan_vi(&a[min], &a[i]);
@@ -66,9 +66,9 @@ void InsertionSort(int a[],int n)
 void InterchangeSort(int a[], int n)
 {
 	int i,j;
-	for(i=1; i<N; i++)
+	for(i=0; i<n-1; i++)
 	{
-		for(j=i+1; j<=N; j++)
+		for(j=i+1; j<n; j++)
 			if(a[j] < a[i])
 			hoan_vi(&a[i], &a[j]);
 			print_array(a,n);
@@ -80,7 +80,7 @@ void InterchangeSort(int a[], int n)
 void BubleSort(int a[], int n)
 {
 	int i,j;
-	for(i=1; i<N-1; i++)
+	for(i=1; i<n-1; i++)
 	{
 		for(j=N-1; j>i; j--)
 			if(a[j] < a[j-1])
@@ -93,7 +93,65 @@ void BubleSort(int a[], int n)
 
 int main()
 {
-	int my_arr[] = {7,8,9,3,15,23,11,27,17,50,22,60};
+	int my_arr[] = {60,68,18,16,54,35,37,27,25,21,18,10};
+	print_array(my_arr,N);
+	
+	while(1)
+	{
+		int my_arr[] = {60,68,18,16,54,35,37,27,25,21,18,10};
+		int lc;
+		printf("0. EXIT\n");
+		printf("1. sap xep chen truc tiep \n");
+		printf("2. sap xep chon truc tiep \n");
+		printf("3. sap xep doi cho truc tiep \n");
+		printf("4. sap xep noi bot \n");
+		printf("--------------------------------\n");
+		printf("moi chon chuc nang \n");
+		scanf("%d",&lc);
+		if(lc == 0)
+		{
+			break;
+			
+		}
+		else if(lc == 1)
+		{
+			InsertionSort(my_arr,N);
+			
+		}
+		else if(lc == 2)
+		{
+			SelectionSort(my_arr,N);
+
+		}
+		else if(lc == 3)
+		{
+			InterchangeSort(my_arr,N);
+			
+		}
+		else if(lc == 4)
+		{
+			BubleSort(my_arr,N);
+			
+		}
+
+
+
+
+
+
+
+
+
+
+
+	}
+
+
+
+
+
+
+	/*int my_arr[] = {7,8,9,3,15,23,11,27,17,50,22,60};
 	print_array(my_arr,N);
 	printf("1. Selection Sort\n");
 	SelectionSort(my_arr,N);
@@ -102,13 +160,12 @@ int main()
 	printf("3. Interchange Sort\n");
 	InterchangeSort(my_arr,N);
 	printf("4. Buble Sort\n");
-	BubleSort(my_arr,N);
+	BubleSort(my_arr,N);*/
 	
 	
 	
-	
-
 	return 0;
 	
 }
 
+	
