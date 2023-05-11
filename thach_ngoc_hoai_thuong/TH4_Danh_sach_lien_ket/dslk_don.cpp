e81030749cd1e38c200dee790988b00aed5cb1ec
phuvinh04 <vinhadmw1@gmail.com>
2023-05-11 15:49:23 +0700

    Them nut cuoi danh sach

---
+++ thach_ngoc_hoai_thuong/TH4_Danh_sach_lien_ket/dslk_don.cpp
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+// struct person with 3 fields
+typedef struct Person
+{
+    int id;
+    char fname[20];
+    char lname[20];
+}Person;
+
+typedef struct Node
+{
+    Person Info;
+    Node* pNext;    
+}Node;
+
+typedef struct List
+{
+    Node* pHead;
+    Node* pTail;
+}List;
+
+Node* GetNode(Person x)
+{
+    Node *p;
+    p = new Node;
+    if (p==NULL)
+    {
+        printf("Khong du bo nho de cap phat cho nut moi");
+        return 0;
+    }
+    p->Info = x;
+    p->pNext = NULL;
+    return p;
+}
+
+void AddFist(List &l, Node* new_ele)
+{
+    if (l.pHead ==NULL)
+    {
+        l.pHead = new_ele;
+        l.pTail = l.pHead;
+    }
+    else
+    {
+        new_ele ->pNext = l.pHead;    
+        l.pHead = new_ele;
+    }
+}
+void Init(List &l)
+{
+    l.pHead = l.pTail = NULL;
+}
+
+
+
+int main()
+{
+    struct Person per1 = {1, "Gia", "Lac" };
+	struct Person per2 = {2, "Hoai", "Thuong" };
+	struct Person per3 = {3, "Ngo", "Ha" };
+    
+    Node* new_ele1  = GetNode(per1);
+    Node* new_ele2  = GetNode(per2);
+    Node* new_ele3  = GetNode(per3);
+    List my_list;
+    Init(my_list);
+
+    AddFist(my_list, new_ele1);
+    AddFist(my_list, new_ele2);
+    AddFist(my_list, new_ele3);
+
+
+}
