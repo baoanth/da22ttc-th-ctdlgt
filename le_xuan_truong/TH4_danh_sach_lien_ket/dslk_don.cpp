3b791ab47e9676fcd0265efb298a9d9fae3293aa
lexuantruongtv <truongle2472004@gmail.com>
2023-05-16 10:36:13 +0700

    Xoa nut nhieu cho

--- le_xuan_truong/TH4_danh_sach_lien_ket/dslk_don.cpp
+++ le_xuan_truong/TH4_danh_sach_lien_ket/dslk_don.cpp
@@ -121,7 +121,34 @@ void RemoveLast(List &l, Node *q)
 		RemoveHead(l);
 }
 	
+void RemoveNode(List &l, int idx)
+{
+	Node *p = l.pHead;
+	Node *q = NULL;
 	
+	while( p != NULL)
+	{
+		if(p->Info.id == idx) break;
+		q = p; 
+		p = p->pNext;
+	}
+	 
+	if(q != NULL)
+	{
+		if(p == l.pTail)
+		l.pTail = q;
+		q->pNext = p->pNext;
+		delete p;
+	}
+	else 
+	{
+		l.pHead = p->pNext;
+		if(l.pHead == NULL)
+			l.pTail = NULL;
+		
+	}
+
+}	
 	
 void Init(List &l)
 {
@@ -163,7 +190,7 @@ int main()
     PrintList(my_list);
     
 	int idx;
-	printf("\nNhap vi tri ID can chen vao : ");
+	printf("\nNhap vi tri ID can chen vao sau no : ");
 	scanf("%d",&idx);
     AddNodeAfter(my_list, idx, new_ele3);
     
@@ -178,10 +205,15 @@ int main()
 	else
 		printf("\nKhong tim thay Node co ID : %d",idx);	
 	
-//	printf("\nSau khi xoa nut dau va cuoi : \n");
+	printf("\nNhap ID can xoa : ");
+	scanf("%d",&idx);
+	
+	printf("\nSau khi xoa ID %d : \n", idx);
 //	RemoveHead(my_list);
 //	RemoveLast(my_list, KQ);
-//	PrintList(my_list);
+	RemoveNode(my_list, idx);
+	PrintList(my_list);
+
 	
 	return 0;
 }
