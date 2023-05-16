5e8f18e7be9f6eb8492ab2c3cb719aaf2abbcdd5
dhphuoc <dhphuoc652003@gmail.com>
2023-05-16 19:05:41 +0700

    finish

--- dang_hoang_phuoc/read_write_text_fle.c
+++ dang_hoang_phuoc/read_write_text_fle.c
@@ -15,17 +15,7 @@ void input_text_file(char* filename){
 	}	
 	fclose(outfile);
 }
-/*char* read_text_file(FILE* infile){
-	char* output_str;
-	char* line;
-	infile = fopen("infile.txt","r");
-	while(!feof(infile)){
-	
-		  line = getline(&line, sizeof(char*line),1, infile);
 
-	}
-	return output_str;
-}*/
 int main(){
 	input_text_file("outfile.txt");
 	return 0;
