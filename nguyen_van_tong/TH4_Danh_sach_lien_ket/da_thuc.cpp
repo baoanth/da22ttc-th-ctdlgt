3ac5d99fe59ba03bf0eb0bb20490faa9a76774b8
TongNguyenvk <tongct08@gmail.com>
2023-05-18 15:10:29 +0700

    da thuc

--- nguyen_van_tong/TH4_Danh_sach_lien_ket/da_thuc.cpp
+++ nguyen_van_tong/TH4_Danh_sach_lien_ket/da_thuc.cpp
@@ -4,97 +4,92 @@
 typedef struct Donthuc
 {
     int heso, bac;
-}donthuc;
-
-
+} donthuc;
 
 typedef struct Node
 {
-	donthuc Info;
-	Node* pNext;
-}Node;
+    donthuc Info;
+    Node *pNext;
+} Node;
 
 typedef struct Dathuc
 {
-	Node* pHead;
-	Node* pTail;
-}dathuc;
+    Node *pHead;
+    Node *pTail;
+} dathuc;
 
-Node* GetNode(donthuc x)
+Node *GetNode(donthuc x)
 {
-	Node *p;
-	p = new Node;
-	if (p==NULL)   
-	{ 
-		printf("Khong du bo nho !"); 
-		return NULL; 
-	}
-	p->Info = x; 
-	p->pNext = NULL;
-	return p; 
+    Node *p;
+    p = new Node;
+    if (p == NULL)
+    {
+        printf("Khong du bo nho !");
+        return NULL;
+    }
+    p->Info = x;
+    p->pNext = NULL;
+    return p;
 }
 
 void Init(dathuc &l)
 {
-	l.pHead = l.pTail = NULL;
+    l.pHead = l.pTail = NULL;
 }
-
 
 void AddTail(dathuc &l, Node *new_ele)
 {
-	if (l.pHead == NULL)
-	{
-		l.pHead = new_ele;
-		l.pTail = l.pHead;
-	}
-	else
-	{
-		l.pTail->pNext = new_ele;
-		l.pTail = new_ele;
-	}
+    if (l.pHead == NULL)
+    {
+        l.pHead = new_ele;
+        l.pTail = l.pHead;
+    }
+    else
+    {
+        l.pTail->pNext = new_ele;
+        l.pTail = new_ele;
+    }
 }
 
 void nhapdathuc(dathuc &l)
 {
     int tmp;
     printf("nhap vao bac da thuc ");
-    scanf("%d",&tmp );
+    scanf("%d", &tmp);
     donthuc x;
-    for(int i = tmp; i>=0; i--)
+    for (int i = tmp; i >= 0; i--)
     {
         printf("He so cua x^%d = ", i);
         scanf("%d", &x.heso);
         x.bac = i;
 
-        Node* p = GetNode(x);
+        Node *p = GetNode(x);
 
-        AddTail(l,p);
+        AddTail(l, p);
     }
 }
 
 void print(dathuc l)
 {
-    Node* p = l.pHead;
-    while(p!=NULL)
+    Node *p = l.pHead;
+    while (p != NULL)
     {
-        if(p != l.pHead && p -> Info.heso > 0)
+        if (p != l.pHead && p->Info.heso > 0)
             printf("+");
-        if(p->Info.bac == 0)
-            printf("%dx",p->Info.heso);
-        printf("%dx^%d",p->Info.heso,p->Info.bac);
+        if (p->Info.bac == 0)
+            printf("%dx", p->Info.heso);
+        printf("%dx^%d", p->Info.heso, p->Info.bac);
         p = p->pNext;
     }
 }
 
-
-
 int main()
 {
-	
+
     dathuc my_dathuc;
     Init(my_dathuc);
 
     nhapdathuc(my_dathuc);
     print(my_dathuc);
-	return 0;
+    return 0;
 }
