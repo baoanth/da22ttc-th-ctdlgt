b47dcb3b6b55bafc1483ba30c1c2a8a2901da868
baoanth <baoanth@gmail.com>
2023-05-18 13:57:22 +0700

    OK

--- nguyen_thi_mong_mo/TH4_DANH_SACH_LIEN_KET/dslk_don.cpp
+++ nguyen_thi_mong_mo/TH4_DANH_SACH_LIEN_KET/dslk_don.cpp
@@ -7,25 +7,25 @@ typedef struct Person
     int id;
     char fname[20];
     char lname[20];
-} Person;
+}Person;
 
 typedef struct Node
 {
     Person Info;
-    Node *pNext;
-} Node;
+    Node* pNext;    
+}Node;
 
 typedef struct List
 {
-    Node *pHead;
-    Node *pTail;
-} List;
+    Node* pHead;
+    Node* pTail;
+}List;
 
-Node *GetNode(Person x)
+Node* GetNode(Person x)
 {
     Node *p;
     p = new Node;
-    if (p == NULL)
+    if (p==NULL)
     {
         printf("Khong du bo nho de cap phat cho nut moi");
         return 0;
@@ -35,23 +35,23 @@ Node *GetNode(Person x)
     return p;
 }
 
-void AddFirst(List &l, Node *new_ele)
+void AddFirst(List &l, Node* new_ele)
 {
-    if (l.pHead == NULL)
+    if (l.pHead ==NULL)
     {
         l.pHead = new_ele;
         l.pTail = l.pHead;      
     }
     else
     {
-        new_ele->pNext = l.pHead;
+        new_ele ->pNext = l.pHead;    
         l.pHead = new_ele;
     }
 }
 
-void AddTail(List &l, Node *new_ele)
+void AddTail(List &l, Node* new_ele)
 {
-    if (l.pHead == NULL)
+    if (l.pHead==NULL)
     {
         l.pHead = new_ele;
         l.pTail = l.pHead;
@@ -63,6 +63,7 @@ void AddTail(List &l, Node *new_ele)
     }
 }
 
+<<<<<<< HEAD
 //Ham nhap Node moi vao vi tri tuy y
 void InputNode(List &l)
 {
@@ -107,93 +108,44 @@ void InputNode(List &l)
 
 
 void RemoveHead(List &l)
+=======
+Node* FindNodeByID(List l, int idx)
+>>>>>>> parent of 31600ad (Add Remove Node)
 {
     Node *p;
-    if (l.pHead != NULL)
-    {
-        p = l.pHead;
-        l.pHead = l.pHead->pNext;
-        delete p;
-        if (l.pHead == NULL)
-            l.pTail = NULL;
-    }
-}
-void RemoveAfter(List &l, Node *q)
-{
-    Node *p;
-    if (q != NULL)
-    {
-        p = q->pNext;
-        if (p != NULL)
-        {
-            if (p == l.pTail)
-                l.pTail = q;
-            q->pNext = p->pNext;
-            delete p;
-        }
-    }
-    else
-        RemoveHead(l);
-}
+    p =l.pHead;
 
-int RemoveNode(List &l, int idx)
-{
-    // Tim nut co id = idx
-    Node *p = l.pHead;
-    Node *q = NULL;
-    while (p != NULL)
-    {
-        if (p->Info.id == idx)
-            break;
-        q = p;
-        p = p->pNext;
-    }
-    if (p == NULL)
-    {
-        return 0; // Không tìm thấy idx
-        printf("Khong tim thay Node co id = %d", idx);
-
-    }
-    if (q != NULL)
-    {
-        if (p == l.pTail) // Nut can xoa la pTail
-            l.pTail = q; 
-        q->pNext = p->pNext;
-        delete p;
-    }
-    else // p là phần tử đầu danh sách
-    {
-        l.pHead = p->pNext;
-        if (l.pHead == NULL)
-            l.pTail = NULL;
-    }
-    return 1;  
-}
-
-Node *FindNodeByID(List l, int idx)
-{
-    Node *p;
-    p = l.pHead;
-
-    while ((p != NULL) && (p->Info.id != idx))
+    while ((p!=NULL) && (p->Info.id !=idx))
         p = p->pNext;
 
     return p;
 }
+
+
 
 void Init(List &l)
 {
     l.pHead = l.pTail = NULL;
 }
 
+<<<<<<< HEAD
+<<<<<<< HEAD
 void PrintNode(Node *p)
 {
-    printf("%5d %20s %20s\n", p->Info.id, p->Info.fname, p->Info.lname);
+    printf("%5d %20s %20s\n", p->Info.id, p->Info.fname, p->Info.lname );
 }
 
+=======
+<<<<<<< HEAD
+>>>>>>> 0c281b7846b87694a39cb32b4f8c6d9e0b7e1017
+=======
+>>>>>>> 53216d064b8a49cd04e413668fa08fdf5874804b
 void PrintList(List &l)
 {
+<<<<<<< HEAD
 
+=======
+>>>>>>> parent of 31600ad (Add Remove Node)
 	if (l.pHead ==NULL)
 	{
 		printf("Danh sach rong\n");
@@ -207,14 +159,22 @@ void PrintList(List &l)
 			printf("%5d %20s %20s\n", p->Info.id, p->Info.fname, p->Info.lname );
 			p = p->pNext;
 		}		
+<<<<<<< HEAD
 	}      
 
+=======
+	}
+>>>>>>> parent of 31600ad (Add Remove Node)
 }
+=======
+>>>>>>> 111ae627302a13f94f32aa31b4a57d5139f9673d
+
 
 
 
 int main()
 {
+<<<<<<< HEAD
     struct Person per1 = {1, "Nguyen", "Le Hoa Binh"};
     struct Person per2 = {2, "Le", "Binh"};
     struct Person per3 = {3, "Tran", "Hoa"};
@@ -233,18 +193,46 @@ int main()
     //AddTail(my_list, new_ele1);
     // AddTail(my_list, new_ele2);
     // AddTail(my_list, new_ele3);
+=======
+    struct Person per1 = {1, "Nguyen", "Le Hoa Binh" };
+	struct Person per2 = {2, "Le", "Binh" };
+	struct Person per3 = {3, "Tran", "Hoa" };
+    
+    Node* new_ele1  = GetNode(per1);
+    Node* new_ele2  = GetNode(per2);
+    Node* new_ele3  = GetNode(per3);
+    List my_list;
+    Init(my_list);
 
+	//AddFirst(my_list, new_ele1);
+    AddFirst(my_list, new_ele2);
+    AddFirst(my_list, new_ele3);
+
+<<<<<<< HEAD
+	
+    AddTail(my_list, new_ele1);
+    //AddTail(my_list, new_ele2);
+    //AddTail(my_list, new_ele3);
+>>>>>>> parent of 31600ad (Add Remove Node)
+
+<<<<<<< HEAD
     PrintList(my_list);
     
 	int idx;
     printf("\nNhap id can tim ");
     scanf("%d", &idx);
+<<<<<<< HEAD
     
 	Node *node_kq = FindNodeByID(my_list, idx);
     if (node_kq != NULL)
+=======
+    Node *node_kq  = FindNodeByID(my_list, idx);
+    if (node_kq!=NULL)
+>>>>>>> parent of 31600ad (Add Remove Node)
         PrintNode(node_kq);
-    else
+    else   
         printf("Tim khong thay node co id %d", idx);
+<<<<<<< HEAD
 
     // PrintList(my_list);
 
@@ -265,6 +253,16 @@ int main()
     InputNode(my_list);
 
     PrintList(my_list);
+=======
+=======
+<<<<<<< HEAD
+    PrintList(my_list);
+>>>>>>> 53216d064b8a49cd04e413668fa08fdf5874804b
+>>>>>>> parent of 31600ad (Add Remove Node)
 
     return 0;
+
+=======
+>>>>>>> 111ae627302a13f94f32aa31b4a57d5139f9673d
+
 }
