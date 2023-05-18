545060b382f6e29291899d0376c375d01031f218
nguyen_hoang_yen <hoangyen24042004@gmail.com>
2023-05-18 13:54:07 +0700

    My frst commit on read_struct.c

--- nguyen_thi_mong_mo/TH4_DANH_SACH_LIEN_KET/dslk_don.cpp
+++ nguyen_thi_mong_mo/TH4_DANH_SACH_LIEN_KET/dslk_don.cpp
@@ -53,7 +53,6 @@ void Init(List &l)
     l.pHead = l.pTail = NULL;
 }
 
-<<<<<<< HEAD
 void PrintList(List &l)
 {
 	if (l.pHead ==NULL)
@@ -71,8 +70,6 @@ void PrintList(List &l)
 		}		
 	}
 }
-=======
->>>>>>> 111ae627302a13f94f32aa31b4a57d5139f9673d
 
 
 int main()
@@ -91,12 +88,9 @@ int main()
     AddFist(my_list, new_ele2);
     AddFist(my_list, new_ele3);
 
-<<<<<<< HEAD
     PrintList(my_list);
 
     return 0;
 
-=======
->>>>>>> 111ae627302a13f94f32aa31b4a57d5139f9673d
 
 }
