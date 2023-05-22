acade370e743108d3a0dbd822bbe391710e718e9
nhutthien <trannhutthien012345@gmail.com>
2023-05-22 14:22:10 +0700

    finish

--- nguyen_thi_huyen_tran/write__struct.c
+++ nguyen_thi_huyen_tran/write__struct.c
@@ -12,9 +12,19 @@ struct person
 };
 int main()
 {
-    printf"hello";
-printf"xinh chao";
-
-
-
+  struct person per1 = {1, "Nguyen", "An"};
+  struct person per2 = {1, "Le", "Binh"};
+  
+  FILE* outfile = fopen("person.dat", "w");
+  
+  fwrite(&per1, sizeof (struct person), 1, outfile);
+  fwrite(&per2, sizeof (struct person), 1, outfile);
+  
+  if (fwrite!=0)
+     printf("file thanh cong");
+  else
+     printf("file that bai");
+  fclose(outfile);
+  
 }
+
