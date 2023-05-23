5c292ef48064d38c71257107ef93630dc45b0b27
lexuantruongtv <truongle2472004@gmail.com>
2023-05-23 09:06:59 +0700

    Cong 2 da thuc

--- le_xuan_truong/TH4_danh_sach_lien_ket/dathuc.cpp
+++ le_xuan_truong/TH4_danh_sach_lien_ket/dathuc.cpp
@@ -91,36 +91,89 @@ void PrintDathuc(Dathuc &l)
 	}
 }
 
-Dathuc CongDathuc(Dathuc l1, Dathuc l2)
+//Dathuc CongDathuc(Dathuc l1, Dathuc l2)
+//{
+//	Node *p, *q;
+//	
+//	Dathuc l_kq;
+//	Init(l_kq);
+//	
+//	Donthuc t;
+//	p= l1.pHead;
+//	q= l2.pHead;
+//	
+//	while(p!= NULL)
+//	{
+//		t.hs= p->Info.hs;
+//		t.bac= p->Info.bac;
+//		
+//		while(q!= NULL)
+//		{
+//			if(p->Info.bac == q->Info.bac)
+//			break;
+//			q= q-> pNext;
+//		}
+//		if(q!= NULL)
+//			t.hs += q->Info.hs;
+//			
+//		Node* new_ele= GetNode(t);
+//		AddTail(l_kq, new_ele);
+//		p=p->pNext;
+//	}
+//	return l_kq;
+//}
+
+void CopyDathuc(Dathuc l, Dathuc &l_kq)
 {
-	Node *p, *q;
+	Node *p;
+	p=l.pHead;
 	
-	Dathuc l_kq;
-	Init(l_kq);
-	
-	Donthuc t;
-	p= l1.pHead;
-	q= l2.pHead;
-	
-	while(p!= NULL)
+	while(p!=NULL)
 	{
-		t.hs= p->Info.hs;
-		t.bac= p->Info.bac;
-		
-		while(q!= NULL)
-		{
-			if(p->Info.bac == q->Info.bac)
-			break;
-			q= q-> pNext;
-		}
-		if(q!= NULL)
-			t.hs += q->Info.hs;
-			
-		Node* new_ele= GetNode(t);
+		Node* new_ele = GetNode(p->Info);
 		AddTail(l_kq, new_ele);
 		p=p->pNext;
+	
 	}
-	return l_kq;
+	printf("\nDa copy Da thuc thanh cong!\n");
+}
+
+Node *TimNodeBacN(Dathuc l, int n)
+{
+	Node *p;
+	p=l.pHead;
+	
+	while(p!=NULL)
+	{
+		if(p->Info.bac==n)
+		break;
+		p=p->pNext;
+	}
+	return p;
+}
+
+void CongDathuc(Dathuc l1, Dathuc l2, Dathuc &l_kq)
+{
+	CopyDathuc(l1,l_kq);
+	
+	Node *p;
+	p=l2.pHead;
+	
+	while(p!=NULL)
+	{
+		Node*FoundNode =TimNodeBacN(l_kq, p->Info.bac);
+		if(FoundNode!=NULL)
+		{
+			FoundNode->Info.hs += p->Info.hs;
+			printf("Tim bac %d\n",p->Info.bac);
+		}
+		else
+		{
+			Node *node_tam= GetNode(p->Info);
+			AddTail(l_kq,node_tam);
+		}
+		p=p->pNext;
+	}
 }
 
 int main()
@@ -129,8 +182,8 @@ int main()
 	Init(my_dathuc1);
 	Dathuc my_dathuc2;
 	Init(my_dathuc2);
-	Dathuc my_dathuc3;
-	
+	Dathuc my_dathuc3;	
+
 	printf("Nhap da thuc 1: ");		
 	NhapDathuc(my_dathuc1);		   
 	PrintDathuc(my_dathuc1);
@@ -141,8 +194,10 @@ int main()
 	NhapDathuc(my_dathuc2);		   
 	PrintDathuc(my_dathuc2);
 	
-	printf("\n\nTong 2 da thuc:  ");	
-	my_dathuc3= CongDathuc(my_dathuc1, my_dathuc2);
+//	printf("\n\nTong 2 da thuc:  ");	
+//	my_dathuc3= CongDathuc(my_dathuc1, my_dathuc2);
+//	PrintDathuc(my_dathuc3);
+	CongDathuc(my_dathuc1, my_dathuc2, my_dathuc3);
 	PrintDathuc(my_dathuc3);
 	
     return 0;
