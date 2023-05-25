446b12fd6622e8baf9ffc09e152c76c569d3af72
NguyenPhucVinh <vinh24122004@gmail.com>
2023-05-25 14:14:32 +0700

    Update

--- nguyen_phuc_vinh/TH5_STACK_QUEUE/stack_char.cpp
+++ nguyen_phuc_vinh/TH5_STACK_QUEUE/stack_char.cpp
@@ -4,7 +4,7 @@
 
 typedef struct Node
 {
-	DonThuc Info;
+	char Info;
 	Node* pNext;
 }Node;
 
@@ -14,7 +14,7 @@ typedef struct Stack
 	Node* pTail;
 }Stack;
 
-Node* Get_Node(Stack x)
+Node* Get_Node(char x)
 {
 	Node *p;
 	p = new Node;
@@ -30,41 +30,95 @@ Node* Get_Node(Stack x)
 	return p;
 }
 
-void Add_First(Stack &s, Node* new_ele)
+void Add_First(Stack &l, Node *new_ele)
 {
-	if(l.pHead==NULL)
-	{
-		l.pHead=new_ele;
-		l.pTail=l.pHead;
-	}
-	else
-	{
+    if (l.pHead == NULL)
+    {
+        l.pHead = new_ele;
+        l.pTail = l.pHead;      
+    }
+    else
+    {
+        new_ele->pNext = l.pHead;
+        l.pHead = new_ele;
+    }
+}
+
+void Remove_Head(Stack &l)
+{
+    Node *p;
+    char x;
+    if (l.pHead != NULL)
+    {
+        p = l.pHead;
+        l.pHead = l.pHead->pNext;
+        delete p;
+        if (l.pHead == NULL)
+            l.pTail = NULL;
+    }
+}
+
+char Is_Empty(Stack &s)
+{
+    if (s.pHead == NULL)
+        return 1;
+    else 
+		return 0;
+}
+
+void Init(Stack &s)
+{
+	s.pHead = NULL;
+	s.pTail = NULL;
+}
+
+void Push(Stack &s, char x)
+{
+    Node* new_ele = Get_Node(x);
+	Add_First(s, new_ele);
+}
+
+char Pop(Stack &s)
+{   
+    char x;
+    if(Is_Empty(s) )
+        return NULL;
     
-	}
+	x = s.pHead->Info;  
+    Remove_Head(s);
+    
+    return x;
 }
 
-void Init(DaThuc &l)
-{
-	l.pHead = l.pTail = NULL;
+char Top(Stack &s)
+{ 
+    if(Is_Empty(s)) 
+        return NULL;
+    return s.pHead->Info;
 }
-
-
-
-char IsEmpty(LIST &S)
-{
-	if(Q.pHead==NULL)
-		return 1;
-	else return 0;
-}
-
-void Push(LIST &S, char x)
-{
-	
-}
-
-
 
 int main()
 {
+	 char  str[] = "EAS*Y**QUE***ST***I*ON";
+    
+    int i; 
+    char x;
+    Stack my_stack;
+    Init(my_stack);
+    
+	printf("Chuoi goc: %s\n", str);
+	printf("\n Ket qua in ra voi STACK:  ");
 	
+    for (i=0; i<strlen(str); i++)
+    {
+        if (str[i]=='*')
+        {
+           x = Pop(my_stack) ;
+           printf("%2c", x);
+        }
+        else
+            Push(my_stack, str[i]);
+    }
+    return 0;
+
 }
