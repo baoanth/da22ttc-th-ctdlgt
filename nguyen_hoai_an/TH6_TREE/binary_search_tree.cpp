c4e5dd4c4de1acd2838c66617dab8fe1f18c7154
annh5393 <annh5393@gmail.com>
2023-06-03 15:24:49 +0700

    update menu lỏ

--- nguyen_hoai_an/TH6_TREE/binary_search_tree.cpp
+++ nguyen_hoai_an/TH6_TREE/binary_search_tree.cpp
@@ -137,14 +137,14 @@ int main()
 	*/
 
     int n, i, x;
-    printf("Nhap so luong tree: ");
+    printf("Nhap so luong phan tu cua cay: ");
     scanf("%d", &n);
     printf("\n");
 
     while(i<n)
     {
         int a[n];
-        printf("Nhap gia tri nut %d : ", i+1);
+        printf("Nhap gia tri %d: ", i+1);
         scanf("%d", &a[i]);
         InsertNode(my_tree, a[i]);
         i++;
@@ -164,8 +164,9 @@ int main()
         printf("|-------------------| \n");
 
         int lua_chon;
-        printf("Chon so: ");
+        printf("Ban chon so: ");
         scanf("%d", &lua_chon);
+        printf("\n");
 
         if (lua_chon == 1)
         {
@@ -189,14 +190,14 @@ int main()
         }
         if (lua_chon == 4)
         {
-            printf("\nNhap x muon them: ");
+            printf("Nhap x muon them: ");
             scanf("%d", &x);
             InsertNode(my_tree, x);
             printf("\n");
         }
         if (lua_chon == 5)
         {
-            printf("\nNhap x muon tim: ");
+            printf("Nhap x muon tim: ");
             scanf("%d", &x);
 
             TNode *gia_tri = SearchNode(my_tree, x);
@@ -208,7 +209,7 @@ int main()
         }
         if (lua_chon == 6)
         {
-            printf("\nNhap x muon xoa: ");
+            printf("Nhap x muon xoa: ");
             scanf("%d", &x);
 
             int kt = DelNode(my_tree, x);
