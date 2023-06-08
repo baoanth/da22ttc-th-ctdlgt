bbbd35f1f2bcc3604745440e92b2fdf177272b48
TongNguyenvk <tongct08@gmail.com>
2023-06-08 19:19:23 +0700

    finish

--- nguyen_van_tong/TH7_quanlysinhvien/masv.cpp
+++ nguyen_van_tong/TH7_quanlysinhvien/masv.cpp
@@ -2,51 +2,72 @@
 #include <string.h>
 #include <stdlib.h>
 
-typedef struct Node {
-    char name[20];
-    char code[20];
-    struct Node* next;
+typedef struct SV
+{
+    char name[10];
+    char code[10];
+} SV;
+
+typedef struct Node
+{
+    SV info;
+    struct Node *next;
 } Node;
 
-Node* creNode(const char* name, const char* code) {
-    Node* p = (Node*)malloc(sizeof(Node));
-    strcpy(p->name, name);
-    strcpy(p->code, code);
+Node *createNode(SV x)
+{
+    Node *p = (Node *)malloc(sizeof(Node));
+    p->info = x;
     p->next = NULL;
     return p;
 }
 
-void addNode(Node* &head, Node* p) {
-    if (head == NULL)
-        head = p;
-    else {
-        Node* q = head;
-        while (q->next != NULL) {
+void addNode(Node **head, Node *p)
+{
+    if (*head == NULL)
+        *head = p;
+    else
+    {
+        Node *q = *head;
+        while (q->next != NULL)
+        {
             q = q->next;
         }
         q->next = p;
     }
 }
 
-Node* loadData(Node* &head,const char* filename) {
-    FILE* infile = fopen(filename, "r");
-    if (infile != NULL) {
+Node *loadData(const char *filename)
+{
+    FILE *infile = fopen(filename, "r");
+    if (infile != NULL)
+    {
+        Node *head = NULL;
         char line[30];
-        while (fgets(line, sizeof(line), infile)) {
-            char code[20];
-            char name[20];
-            sscanf(line, "%[^,], %[^\n]", code, name);
-            Node* tmp = creNode(name, code);
-            addNode(head, tmp);
+        while (fgets(line, sizeof(line), infile))
+        {
+            char *token;
+            SV x;
+
+            token = strtok(line, ",");
+            strcpy(x.code, token);
+            token = strtok(NULL, ",");
+            strcpy(x.name, token);
+
+            Node *tmp = createNode(x);
+            addNode(&head, tmp);
         }
         fclose(infile);
         return head;
     }
-    return NULL;
+    else 
+        return NULL;
 }
 
-void findStudentInfo(Node* bacList, Node* heList, Node* nganhList, Node* khoaList, const char* studentID) {
-    if (strlen(studentID) != 9) {
+void findStudentInfo(Node *bacList, Node *heList, Node *nganhList, Node *khoaList, const char *studentID)
+{
+    if (strlen(studentID) != 9)
+    {
         printf("Ma so sinh vien khong hop le\n");
         return;
     }
@@ -67,56 +88,66 @@ void findStudentInfo(Node* bacList, Node* heList, Node* nganhList, Node* khoaLis
     strncpy(khoaCode, studentID + 4, 2);
     khoaCode[2] = '\0';
 
-    Node* bacNode = bacList;
-    while (bacNode != NULL) {
-        if (strcmp(bacNode->code, bacCode) == 0) {
+    Node *bacNode = bacList;
+    while (bacNode != NULL)
+    {
+        if (strcmp(bacNode->info.code, bacCode) == 0)
+        {
             break;
         }
         bacNode = bacNode->next;
     }
 
-    Node* heNode = heList;
-    while (heNode != NULL) {
-        if (strcmp(heNode->code, heCode) == 0) {
+    Node *heNode = heList;
+    while (heNode != NULL)
+    {
+        if (strcmp(heNode->info.code, heCode) == 0)
+        {
             break;
         }
         heNode = heNode->next;
     }
 
-    Node* nganhNode = nganhList;
-    while (nganhNode != NULL) {
-        if (strcmp(nganhNode->code, nganhCode) == 0) {
+    Node *nganhNode = nganhList;
+    while (nganhNode != NULL)
+    {
+        if (strcmp(nganhNode->info.code, nganhCode) == 0)
+        {
             break;
         }
         nganhNode = nganhNode->next;
     }
 
-    Node* khoaNode = khoaList;
-    while (khoaNode != NULL) {
-        if (strcmp(khoaNode->code, khoaCode) == 0) {
+    Node *khoaNode = khoaList;
+    while (khoaNode != NULL)
+    {
+        if (strcmp(khoaNode->info.code, khoaCode) == 0)
+        {
             break;
         }
         khoaNode = khoaNode->next;
     }
 
-    if (bacNode == NULL || heNode == NULL || nganhNode == NULL || khoaNode == NULL) {
+    if (bacNode == NULL || heNode == NULL || nganhNode == NULL || khoaNode == NULL)
+    {
         printf("Khong tim thay thong tin sinh vien\n");
         return;
     }
 
-    printf("%s %s %s Khoa %s\n", heNode->name, bacNode->name, nganhNode->name, khoaNode->name);
+    printf("%s %s %s Khoa %s\n", heNode->info.name, bacNode->info.name, nganhNode->info.name, khoaNode->info.name);
 }
 
-int main() {
-    Node* bac = NULL;
-    Node* khoa = NULL;
-    Node* nganh = NULL;
-    Node* he = NULL;
+int main()
+{
+    Node *bac = NULL;
+    Node *khoa = NULL;
+    Node *nganh = NULL;
+    Node *he = NULL;
 
-    bac = loadData(bac,"bac.txt");
-    khoa = loadData(khoa,"khoa.txt");
-    nganh = loadData(nganh,"nganh.txt");
-    he = loadData(he,"he.txt");
+    bac = loadData("bac.txt");
+    khoa = loadData("khoa.txt");
+    nganh = loadData("nganh.txt");
+    he = loadData("he.txt");
 
     char ma[10];
     printf("Nhap ma sinh vien: ");
