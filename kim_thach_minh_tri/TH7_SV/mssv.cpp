7b6b54584d47b35b6b4e9364667bfa93d8ae8d1c
kimthachminhtri <trikimfa1904@gmail.com>
2023-06-10 15:19:48 +0700

    UPDATE

--- kim_thach_minh_tri/TH7_SV/mssv.cpp
+++ kim_thach_minh_tri/TH7_SV/mssv.cpp
@@ -143,6 +143,69 @@ void LoadData(char *filename, List &l)
 }
 
 
+void FindByID(List l_bac, List l_he, List l_nganh, List l_khoa, char *id)
+{
+   
+    char bac[2], he[2], nganh[3], khoa[3], id_sv[4];
+    strncpy(bac, id, 1);
+    bac[1] = '\0';
+    strncpy(he, id + 1, 1);
+    he[1] = '\0';
+    strncpy(nganh, id + 2, 2);
+    nganh[2] = '\0';
+    strncpy(khoa, id + 4, 2);
+    khoa[2] = '\0';
+    strncpy(id_sv, id + 6, 3);
+    id_sv[3] = '\0';
+
+   
+    Node *p_bac = l_bac.pHead;
+    while (p_bac != NULL)
+    {
+        if (strcmp(p_bac->Info.ma, bac) == 0)
+        {
+            Node *p_he = p_bac->pNext;
+            while (p_he != NULL)
+            {
+                if (strcmp(p_he->Info.ma, he) == 0)
+                {
+                    Node *p_nganh = p_he->pNext;
+                    while (p_nganh != NULL)
+                    {
+                        if (strcmp(p_nganh->Info.ma, nganh) == 0)
+                        {
+                            Node *p_khoa = p_nganh->pNext;
+                            while (p_khoa != NULL)
+                            {
+                                if (strcmp(p_khoa->Info.ma, khoa) == 0)
+                                {
+                                    Node *p_sv = p_khoa->pNext;
+                                    while (p_sv != NULL)
+                                    {
+                                        if (strcmp(p_sv->Info.ma, id_sv) == 0)
+                                        {
+                                            printf("Ma so sinh vien: %s\n", id);
+                                            printf("Ten sinh vien: %s\n", p_sv->Info.ten);
+                                            return;
+                                        }
+                                        p_sv = p_sv->pNext;
+                                    }
+                                }
+                                p_khoa = p_khoa->pNext;
+                            }
+                        }
+                        p_nganh = p_nganh->pNext;
+                    }
+                }
+                p_he = p_he->pNext;
+            }
+        }
+        p_bac = p_bac->pNext;
+    }
+    printf("Khong tim thay sinh vien co ma so %s\n", id);
+}
+
+
 int main()
 {
     List list_bac, list_khoa, list_he, list_nganh;
@@ -163,6 +226,13 @@ int main()
     PrintList(list_he);
     printf("==========================================\n");
     PrintList(list_nganh);
+
+	char id[10];
+
+    printf("Nhap id : ");
+    scanf("%s", &id);
+    
+    FindByID(list_bac, list_he, list_nganh, list_khoa, id);
     
     return 0;
 }
