dd145b6db816fe48dc539f56705edd3d58e560fb
nugyenketong <nguyenketong1603@gmail.com>
2023-06-12 14:47:18 +0700

    phan tich mssv

---
+++ Nguyen_Ke_Tong/TH4_DANH_SACH_LIEN_KET/da_thuc.cpp
@@ -0,0 +1,150 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct DonThuc
+{
+    int heso;
+    int bac;
+}DonThuc;
+
+typedef struct Node
+{
+    DonThuc Info;
+    Node* pNext;
+}Node;
+
+typedef struct DaThuc
+{
+    Node *pHead;
+    Node *pTail;
+}DaThuc;
+
+Node *GetNode (DonThuc x)
+{
+    Node *p;
+    p= new Node;
+    if (p==NULL)
+    {
+        printf("Khong du bo nho de cap nhat nut moi");
+        return NULL;
+    }
+    p->Info = x;
+    p->pNext = NULL;
+    return p;
+}
+
+void AddTail(DaThuc &l, Node *new_ele)
+{
+    if(l.pHead == NULL)
+    {
+        l.pHead = new_ele;
+        l.pTail = l.pHead;
+    }
+    else
+    {
+        l.pTail->pNext = new_ele;
+        l.pTail = new_ele;
+    }
+}
+
+void NhapDathuc(DaThuc &l)
+{
+	int MaxN= 0;
+	int i,j;
+	DonThuc s;
+	
+	printf("PT co bac: ");
+	scanf("%d", &MaxN);
+	
+	for(i= MaxN; i>=0; i--)
+	{
+		printf("x^%d = ",i);
+		s.bac= i;
+		scanf("%d", &s.heso);
+		
+		Node* new_ele = GetNode(s);
+		AddTail(l, new_ele);	
+	}
+}
+
+void Init(DaThuc &l)
+{
+    l.pHead = l.pTail = NULL;
+}
+
+void PrintDaThuc(DaThuc &l)
+{
+    Node *p;
+	p = l.pHead;
+	
+	printf("\nP(x)= ");
+	
+    while (p!= NULL)
+    {
+        
+        if((p->Info.heso>0)&&(p!= l.pTail))
+        printf("+");
+        
+        if(p->Info.bac == 0)
+        printf("%d",p->Info.heso);
+        
+        else 
+        printf("%dx^%d",p->Info.heso, p->Info.bac);
+
+        p= p->pNext;
+    }
+}
+
+DaThuc CongDaThuc(Dathuc l1, Dathuc l2)
+{
+	Node *p, *q;
+	Dathuc l_kq;
+	Init (l_kq);
+	Donthuc dathuc_tam;
+	p= l1.pHead;
+	q= l2.pHead;
+	while(p!=NULL)
+	{
+		dathuc_tam.hs=p->Info.hs;
+		dathuc_tam.bac=p->Info.bac;
+		while(q!=NULL)
+		{
+			if(p->Info.bac == q->Info.bac)
+			break;
+			q=q->pNext;
+		}
+		if(q!=NULL)
+			dathuc_tam.hs += q->Info.hs;
+			
+		Node* new_ele = GetNode(dathuc_tam);
+		AddTail(l_kq, new_ele);
+		
+		p=p->pNext;
+	}
+	
+	return l_kq;
+}
+
+int main()
+{
+    DaThuc dt1, dt2, dt3;
+	Init(dt1);
+	Init(dt2);
+	
+	printf("Nhap da thuc 1: \n");
+	NhapDaThuc(dt1);
+	PrintDathuc(dt1);
+	
+	printf("\n\n");
+	 
+	printf("Nhap da thuc 2: \n");
+	NhapDaThuc(dt2);
+	PrintDathuc(dt2);
+	
+	dt3 = CongDaThuc(dt1, dt2);
+	printf("\n\nTong hai da thuc la:  ");
+	PrintDathuc(dt3);
+	
+    
+    return 0;
+}
---
+++ Nguyen_Ke_Tong/TH_8_phantichsv/phantichmssv.cpp
@@ -0,0 +1,171 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Node 
+{
+    char mssv[3];
+    char ten[50];
+    struct Node* next;
+};
+
+struct LinkedList 
+{
+    struct Node* head;
+};
+
+void addNode(struct LinkedList* list, char mssv[], char ten[]) 
+{
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    strcpy(newNode->mssv, mssv);
+    strcpy(newNode->ten, ten);
+    newNode->next = NULL;
+
+    if (list->head == NULL) 
+	{
+        list->head = newNode;
+    }
+    else 
+	{
+        struct Node* current = list->head;
+        while (current->next != NULL) 
+		{
+            current = current->next;
+        }
+        current->next = newNode;
+    }
+}
+
+void printList(struct LinkedList* list) 
+{
+    struct Node* current = list->head;
+    while (current != NULL) 
+	{
+        printf("%s, %s\n", current->mssv, current->ten);
+        current = current->next;
+    }
+}
+
+char* getNameByCode(struct LinkedList* list, char mssv[]) 
+{
+    struct Node* current = list->head;
+    while (current != NULL) 
+	{
+        if (strcmp(current->mssv, mssv) == 0) 
+		{
+            return current->ten;
+        }
+        current = current->next;
+    }
+    return "Uknown";
+}
+
+void load_data(struct LinkedList* nganhList, struct LinkedList* bacList, struct LinkedList* heList, struct LinkedList* khoaList) 
+{
+    FILE* bacFile = fopen("bac.txt", "r");
+    if (bacFile != NULL) 
+	{
+        char line[100];
+        while (fgets(line, sizeof(line), bacFile)) 
+		{
+            char mssv[3], ten[50];
+            sscanf(line, "%[^,], %[^\n]", mssv, ten);
+            addNode(bacList, mssv, ten);
+        }
+        fclose(bacFile);
+    }
+     FILE* heFile = fopen("he.txt", "r");
+    if (heFile != NULL) 
+	
+	{
+        char line[100];
+        while (fgets(line, sizeof(line), heFile)) 
+		{
+            char mssv[3], ten[50];
+            sscanf(line, "%[^,], %[^\n]", mssv, ten);
+            addNode(heList, mssv, ten);
+        }
+        fclose(heFile);
+    }
+
+    FILE* nganhFile = fopen("nganh.txt", "r");
+    if (nganhFile != NULL) 
+	{
+        char line[100];
+        while (fgets(line, sizeof(line), nganhFile)) 
+		{
+            char mssv[3], ten[50];
+            sscanf(line, "%[^,], %[^\n]", mssv, ten);
+            addNode(nganhList, mssv, ten);
+        }
+        fclose(nganhFile);
+    }
+
+    FILE* khoaFile = fopen("khoa.txt", "r");
+    if (khoaFile != NULL)
+	 {
+        char line[100];
+        while (fgets(line, sizeof(line), khoaFile)) 
+		{
+            char mssv[3], ten[50];
+            sscanf(line, "%[^,], %[^\n]", mssv, ten);
+            addNode(khoaList, mssv, ten);
+        }
+        fclose(khoaFile);
+    }
+}
+
+void get_student_info(char studentCode[], struct LinkedList* nganhList, struct LinkedList* bacList, struct LinkedList* heList, struct LinkedList* khoaList) 
+{
+    char bacCode[3];
+    strncpy(bacCode, studentCode, 1);
+    bacCode[1] = '\0';
+
+    char heCode[3];
+    strncpy(heCode, studentCode + 1, 1);
+    heCode[1] = '\0';
+
+    char nganhCode[3];
+    strncpy(nganhCode, studentCode + 2, 2);
+    nganhCode[2] = '\0';
+
+    char khoaCode[3];
+    strncpy(khoaCode, studentCode + 4, 2);
+    khoaCode[2] = '\0';
+
+    char id[4];
+    strncpy(id, studentCode + 6, 3);
+    id[3] = '\0';
+
+    char* bacName = getNameByCode(bacList, bacCode);
+    char* heName = getNameByCode(heList, heCode);
+    char* nganhName = getNameByCode(nganhList, nganhCode);
+    char* khoaName = getNameByCode(khoaList, khoaCode);
+
+    printf("%s %s %s Khoa %s \n", bacName, heName, nganhName, khoaName, id);
+}
+
+int main() 
+{
+    struct LinkedList nganhList, bacList, heList, khoaList;
+    nganhList.head = NULL;
+    bacList.head = NULL;
+    heList.head = NULL;
+    khoaList.head = NULL;
+
+    load_data(&nganhList, &bacList, &heList, &khoaList);
+
+    char studentCode[10];
+    printf("Nhap ma so sinh vien: ");
+    scanf("%s", studentCode);
+
+    get_student_info(studentCode, &nganhList, &bacList, &heList, &khoaList);
+
+    return 0;
+}
+
+
+
+
+
+
