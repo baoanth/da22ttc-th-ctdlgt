711a54cb275ae07a72cbd5b29bfe8298233d2baf
kimthachminhtri <trikimfa1904@gmail.com>
2023-06-17 14:21:17 +0700

    UPDATE

--- kim_thach_minh_tri/TH9_HASH_TABLE/double_hashing.cpp
+++ kim_thach_minh_tri/TH9_HASH_TABLE/double_hashing.cpp
@@ -2,53 +2,67 @@
 
 #define TABLE_SIZE 10
 
-int hash1(int key) {
+int hash1(int key)
+{
     return key % TABLE_SIZE;
 }
 
-int hash2(int key) {
+int hash2(int key)
+{
     return 7 - (key % 7);
 }
 
-int doubleHashing(int key, int attempt) {
+int doubleHashing(int key, int attempt)
+{
     return (hash1(key) + attempt * hash2(key)) % TABLE_SIZE;
 }
 
-void insert(int table[], int key) {
+void insert(int table[], int key)
+{
     int attempt = 0;
     int index;
-	printf("Thu chen  = %d\n", key);
-    do {
+    printf("Thu chen  = %d\n", key);
+    do
+    {
         index = doubleHashing(key, attempt);
         printf("  Index = %d\n", index);
-        if (table[index] == -1) {
+        if (table[index] == -1)
+        {
             table[index] = key;
             break;
         }
         attempt++;
     } while (attempt < TABLE_SIZE);
 
-    if (attempt == TABLE_SIZE) {
+    if (attempt == TABLE_SIZE)
+    {
         printf("Bang ban day. Khong the chen gia tri %d.\n", key);
     }
 }
 
-void printHashTable(int table[]) {
+void printHashTable(int table[])
+{
     printf("Bảng băm:\n");
-    for (int i = 0; i < TABLE_SIZE; i++) {
-        if (table[i] != -1) {
+    for (int i = 0; i < TABLE_SIZE; i++)
+    {
+        if (table[i] != -1)
+        {
             printf("[%d]: %d\n", i, table[i]);
-        } else {
+        }
+        else
+        {
             printf("[%d]: __\n", i);
         }
     }
 }
 
-int main() {
+int main()
+{
     int hashTable[TABLE_SIZE];
 
     // Khởi tạo bảng băm
-    for (int i = 0; i < TABLE_SIZE; i++) {
+    for (int i = 0; i < TABLE_SIZE; i++)
+    {
         hashTable[i] = -1;
     }
 
