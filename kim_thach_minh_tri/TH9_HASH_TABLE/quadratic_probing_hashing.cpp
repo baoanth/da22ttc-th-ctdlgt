309cc533f552f8332e748e41164f7cbd7051d66e
kimthachminhtri <trikimfa1904@gmail.com>
2023-06-17 15:14:00 +0700

    UPDATE

--- kim_thach_minh_tri/TH9_HASH_TABLE/quadratic_probing_hashing.cpp
+++ kim_thach_minh_tri/TH9_HASH_TABLE/quadratic_probing_hashing.cpp
@@ -2,22 +2,26 @@
 
 #define TABLE_SIZE 29
 
-int hash(int key) {
+int hash(int key)
+{
     return key % TABLE_SIZE;
 }
 
-void insert(int table[], int key) {
+void insert(int table[], int key)
+{
     printf("Key = %d\n", key);
     int index = hash(key);
     printf("    Index1 = %d\n", index);
-    
+
     int i = 0;
 
-    while (table[index] != -1) {
+    while (table[index] != -1)
+    {
         i++;
         index = (hash(key) + i * i) % TABLE_SIZE;
-        printf("    Index%d = %d\n",i+1, index);
-        if (i >= TABLE_SIZE) {
+        printf("    Index%d = %d\n", i + 1, index);
+        if (i >= TABLE_SIZE)
+        {
             printf("Bảng băm đầy. Không thể chèn giá trị %d.\n", key);
             return;
         }
@@ -26,22 +30,29 @@ void insert(int table[], int key) {
     table[index] = key;
 }
 
-void printHashTable(int table[]) {
+void printHashTable(int table[])
+{
     printf("Bảng băm:\n");
-    for (int i = 0; i < TABLE_SIZE; i++) {
-        if (table[i] != -1) {
+    for (int i = 0; i < TABLE_SIZE; i++)
+    {
+        if (table[i] != -1)
+        {
             printf("[%d]: %d\n", i, table[i]);
-        } else {
+        }
+        else
+        {
             printf("[%d]: __\n", i);
         }
     }
 }
 
-int main() {
+int main()
+{
     int hashTable[TABLE_SIZE];
 
     // Khởi tạo bảng băm
-    for (int i = 0; i < TABLE_SIZE; i++) {
+    for (int i = 0; i < TABLE_SIZE; i++)
+    {
         hashTable[i] = -1;
     }
 
